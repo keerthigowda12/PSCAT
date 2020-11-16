deba59674651216f90bdb8c467be6cc87bab9326
Saurav Sah <Saurav.Sah@powerschool.com>
2020-11-16 15:01:06 +0530

    Student Layout

---
+++ PSCAT/Controllers/StudentsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PSCAT.Controllers
+{
+    public class StudentsController : Controller
+    {
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+        public IActionResult Tests()
+        {
+            return View();
+        }
+        public IActionResult ImproveMe()
+        {
+            return View();
+        }
+    }
+}
---
+++ PSCAT/Models/StudentScore.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PSCAT.Models
+{
+    public class StudentScore
+    {
+        [Required]
+        public string StudentID { get; set; }
+
+        [Required]
+        public string TestID { get; set; }
+
+        [Required]
+        [Display(Name = "Score")]
+        public bool MarksScored { get; set; }
+    }
+}
