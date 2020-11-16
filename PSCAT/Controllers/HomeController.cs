5a6de0c5fe465f90e232e55d7eeb4b58cdd1ad7a
Keerthi SN <Keerthi.SN@powerschool.com>
2020-11-16 11:58:59 +0530

    Add project files.

---
+++ PSCAT/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PSCAT.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PSCAT.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+       
+        [HttpPost]
+        public IActionResult Login(LoginViewModel viewModel)
+        {
+            if (viewModel.User == "admin")
+            {
+                 //Redirect("~/Home/TeacherPD");
+            }
+            else
+            {
+                //Redirect("~/Home/TeacherPD");
+            }
+
+            return (IActionResult)viewModel;
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ PSCAT/Models/LoginViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PSCAT.Models
+{
+    public class LoginViewModel
+    {
+        [Required]
+        public string User { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Display(Name = "Remember Me")]
+        [DataType(DataType.Password)]
+        public bool RememberMe { get; set; }
+    }
+}
